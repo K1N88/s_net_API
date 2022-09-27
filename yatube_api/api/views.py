a8bad57eb9a9b376eaebdc5b38691e1fd5a50487
K1N88 <105746796+K1N88@users.noreply.github.com>
2022-09-27 22:21:10 +0300

    Update views.py

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework import filters
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import LimitOffsetPagination
 
 from posts.models import Post, Group
